2dd753ea2d9aba1a6244fc3ff5e83054cca89240
miguel977 <miguel645@gmail.com>
2025-03-11 08:39:19 -0300

    Trabalhando com formulário class Based Views teste

--- core/models.py
+++ core/models.py
@@ -3,10 +3,12 @@ from django.db import models
 
 from stdimage.models import StdImageField
 
+
 def get_file_path(_instance, filename):
     ext = filename.split('.')[-1]
     filename = f'{uuid.uuid4()}.{ext}'
     return filename
+
 
 class Base(models.Model):
     criados = models.DateField('Criação', auto_now_add=True)
@@ -15,7 +17,8 @@ class Base(models.Model):
 
     class Meta:
         abstract = True
-        
+
+
 class Servico(Base):
     ICONE_CHOICES = (
         ('lni-cog', 'Engrenagem'),
@@ -35,7 +38,8 @@ class Servico(Base):
 
     def __str__(self):
         return self.servico
-    
+
+
 class Cargo(Base):
     cargo = models.CharField('Cargo', max_length=100)
 
@@ -46,11 +50,12 @@ class Cargo(Base):
     def __str__(self):
         return self.cargo
 
+
 class Funcionario(Base):
     nome = models.CharField('Nome', max_length=100)
     cargo = models.ForeignKey('core.Cargo', verbose_name='Cargo', on_delete=models.CASCADE)
     bio = models.TextField('Bio', max_length=200)
-    imagem = StdImageField('Imagem', upload_to='get_file_path', variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
+    imagem = StdImageField('Imagem', upload_to=get_file_path, variations={'thumb': {'width': 480, 'height': 480, 'crop': True}})
     facebook = models.CharField('Facebook', max_length=100, default='#')
     twitter = models.CharField('Twitter', max_length=100, default='#')
     instagram = models.CharField('Instagram', max_length=100, default='#')
@@ -61,3 +66,6 @@ class Funcionario(Base):
 
     def __str__(self):
         return self.nome
+
+
+
