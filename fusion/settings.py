939a81990b1b527ab116f9f98c4bf486523cb35d
miguel977 <miguel645@gmail.com>
2025-03-12 08:18:48 -0300

    arquivo do heroku

--- fusion/settings.py
+++ fusion/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import os
+import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -73,7 +75,7 @@ WSGI_APPLICATION = "fusion.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
+"""
 DATABASES = {
     "default": {
         'ENGINE': 'django.db.backends.postgresql',
@@ -84,11 +86,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+"""
 
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
-
+DATABASES = {
+    'default': dj_database_url.config()
+    }
 AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
@@ -128,8 +133,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #Email teste console
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+# Email teste console
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 """
+
 # Email produção
 
 EMAIL_HOST = 'localhost'
@@ -143,3 +151,5 @@ DEFAULT_FROM_EMAIL = 'contato@fusion.com.br'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGOUT_REDIRECT_URL = 'index'
